acfb1f436c7d59e07100079f867693606d48d029
Nikita Prekrasnyi <topdoterinua@gmail.com>
2022-04-29 21:16:18 +0300

    init

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  $('.header-navbar__btn').on('click', function () {
+    $(this).toggleClass('header-navbar__btn--active');
+    $('.header-navbar__list').stop(true, true).slideToggle(500);
+  });
+});
